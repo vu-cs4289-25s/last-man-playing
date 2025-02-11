98582f73d842c8a2e60319872cf845d06c56b113
JonathanTilahun <JonathanTilahun+username@users.noreply.github.com>
2025-02-11 17:01:13 -0600

    EC3 instance, S3 bucket, and DB initial set up

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Create a VPC for networking
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2, 
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //S3 Bucket for static files
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true, 
+    });
+
+    //RDS PostgreSQL database instance
+    const dbInstance = new rds.DatabaseInstance(this, 'PostgresDB', {
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_13,
+      }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO
+      ),
+      vpc,
+      allocatedStorage: 20, // 20 GB of storage
+      maxAllocatedStorage: 100, // Allow it to grow up to 100 GB
+      credentials: rds.Credentials.fromGeneratedSecret('postgres'), // Auto-generate DB credentials
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Destroy the DB when the stack is destroyed (for development only)
+      deletionProtection: false, // Disable deletion protection (for development)
+    });
+
+    //EC2 instance
+    const ec2Instance = new ec2.Instance(this, 'MyEC2', {
+      instanceType: new ec2.InstanceType('t3.micro'),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+      vpc,
+    });
   }
 }
